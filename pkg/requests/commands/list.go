089e2c2db6308eae457705dfef12d5a23751e3b9
Asaf Alima <asaf.alima@gmail.com>
2023-03-14 00:06:09 -0300

    DEV-6180 Add specific access request status

--- pkg/requests/commands/list.go
+++ pkg/requests/commands/list.go
@@ -3,15 +3,17 @@ package commands
 import (
 	"context"
 	"fmt"
-	"github.com/apono-io/apono-cli/pkg/aponoapi"
 	"strings"
 
 	"github.com/gosuri/uitable"
 	"github.com/spf13/cobra"
+
+	"github.com/apono-io/apono-cli/pkg/aponoapi"
 )
 
 func List() *cobra.Command {
 	var daysOffset int64
+	var requestID string
 
 	cmd := &cobra.Command{
 		Use:     "requests",
@@ -23,39 +25,74 @@ func List() *cobra.Command {
 				return err
 			}
 
-			requests, err := listRequests(cmd.Context(), client, daysOffset)
-			if err != nil {
-				return err
+			if requestID != "" {
+				return showRequestStatus(cmd, client, requestID)
 			}
 
-			integrations, err := listIntegrations(cmd.Context(), client)
-			if err != nil {
-				return err
-			}
-
-			table := uitable.New()
-			table.AddRow("REQUEST ID", "INTEGRATION", "RESOURCES", "PERMISSIONS", "STATUS")
-			for _, request := range requests {
-				integrationID := request.IntegrationId
-				integration := integrationID
-				if integrationName, found := integrations[integrationID]; found {
-					integration = integrationName
-				}
-
-				resourceIds := strings.Join(request.ResourceIds, ", ")
-				permissions := strings.Join(request.Permissions, ", ")
-				table.AddRow(request.FriendlyRequestId, integration, resourceIds, permissions, request.Status)
-			}
-
-			_, err = fmt.Fprintln(cmd.OutOrStdout(), table)
-			return err
+			return showRequestsSummary(cmd, client, daysOffset)
 		},
 	}
 
 	flags := cmd.Flags()
 	flags.Int64VarP(&daysOffset, "days", "d", 7, "number of days to list")
+	flags.StringVarP(&requestID, "id", "i", "", "specific request id")
 
 	return cmd
+}
+
+func showRequestStatus(cmd *cobra.Command, client *aponoapi.AponoClient, requestID string) error {
+	resp, err := client.GetAccessRequestWithResponse(cmd.Context(), requestID)
+	if err != nil {
+		return err
+	}
+
+	accessRequest := resp.JSON200
+	integrationID := accessRequest.IntegrationId
+	integrationResp, err := client.GetIntegrationV2WithResponse(cmd.Context(), integrationID)
+	if err != nil {
+		return err
+	}
+
+	table := uitable.New()
+	table.Wrap = true
+	table.AddRow("ID:", accessRequest.FriendlyRequestId)
+	table.AddRow("Status:", accessRequest.Status)
+	table.AddRow("Integration:", integrationResp.JSON200.Name)
+	table.AddRow("Resources:", strings.Join(accessRequest.ResourceIds, ", "))
+	table.AddRow("Permissions:", strings.Join(accessRequest.Permissions, ", "))
+	table.AddRow("Justification:", accessRequest.Justification)
+
+	_, err = fmt.Fprintln(cmd.OutOrStdout(), table)
+	return err
+}
+
+func showRequestsSummary(cmd *cobra.Command, client *aponoapi.AponoClient, daysOffset int64) error {
+	requests, err := listRequests(cmd.Context(), client, daysOffset)
+	if err != nil {
+		return err
+	}
+
+	integrations, err := listIntegrations(cmd.Context(), client)
+	if err != nil {
+		return err
+	}
+
+	table := uitable.New()
+	table.AddRow("REQUEST ID", "INTEGRATION", "RESOURCES", "PERMISSIONS", "STATUS")
+	for _, request := range requests {
+		integrationID := request.IntegrationId
+		integration := integrationID
+		if integrationName, found := integrations[integrationID]; found {
+			integration = integrationName
+		}
+
+		resourceIds := strings.Join(request.ResourceIds, ", ")
+		permissions := strings.Join(request.Permissions, ", ")
+		table.AddRow(fmt.Sprintf("%s (%s)", request.FriendlyRequestId, request.RequestId), integration, resourceIds, permissions, request.Status)
+	}
+
+	_, err = fmt.Fprintln(cmd.OutOrStdout(), table)
+	return err
 }
 
 func listRequests(ctx context.Context, client *aponoapi.AponoClient, daysOffset int64) ([]aponoapi.AccessRequest, error) {
