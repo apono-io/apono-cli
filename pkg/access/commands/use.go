d537b0c4b9a89bdbf516f764f748e192ff77066e
naorgu <naor@apono.io>
2023-12-17 13:24:48 +0200

    pr comments

--- pkg/access/commands/use.go
+++ pkg/access/commands/use.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -34,15 +35,36 @@ func AccessDetails() *cobra.Command {
 			var output string
 			switch outputFormat {
 			case "cli":
-				output = accessDetails.GetCli()
+				link := accessDetails.GetCli()
+				if accessDetails.GetCli() == "" {
+					return errors.New("no output for format: cli")
+				}
+				output = link
 			case "link":
 				link := accessDetails.GetLink()
+				if link.GetUrl() == "" {
+					return errors.New("no output for format: link")
+				}
 				output = link.GetUrl()
 			case "instructions":
+				if accessDetails.Instructions.Plain == "" {
+					return errors.New("no output for format: instructions")
+				}
 				output = accessDetails.Instructions.Plain
 			case "json":
-				outputBytes, _ := json.Marshal(accessDetails)
+				if accessDetails.Credentials == nil {
+					return errors.New("no output for format: json")
+				}
+				var outputBytes []byte
+				outputBytes, err = json.Marshal(accessDetails.Credentials)
+				if err != nil {
+					return err
+				}
 				output = string(outputBytes)
+			}
+
+			if output == "" {
+				return fmt.Errorf("no output for format: %s", outputFormat)
 			}
 
 			_, err = fmt.Fprintln(cmd.OutOrStdout(), output)
