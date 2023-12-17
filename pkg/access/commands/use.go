48c1d739037175e39000d71875d5a12524f100f4
naorgu <naor@apono.io>
2023-12-17 16:10:11 +0200

    support --run flag for `apono access use`

--- pkg/access/commands/use.go
+++ pkg/access/commands/use.go
@@ -1,33 +1,51 @@
 package commands
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"os/exec"
+	"strings"
 
 	"github.com/spf13/cobra"
 
 	"github.com/apono-io/apono-cli/pkg/aponoapi"
+	"github.com/apono-io/apono-cli/pkg/clientapi"
 )
 
-const outputFlagName = "output"
+const (
+	outputFlagName  = "output"
+	executeFlagName = "run"
+)
 
 func AccessDetails() *cobra.Command {
 	var outputFormat string
+	var execute bool
 
 	cmd := &cobra.Command{
 		Use:     "use [id]",
 		GroupID: Group.ID,
 		Short:   "Get access session details",
-		Args:    cobra.ExactArgs(1), // This will enforce that exactly 1 argument must be provided
+		Args:    cobra.MinimumNArgs(1), // This will enforce that exactly 1 argument must be provided
 		RunE: func(cmd *cobra.Command, args []string) error {
 			client, err := aponoapi.GetClient(cmd.Context())
 			if err != nil {
 				return err
 			}
 
-			id := args[0]
-			accessDetails, _, err := client.ClientAPI.AccessSessionsAPI.GetAccessSessionAccessDetails(cmd.Context(), id).Execute()
+			accessID := args[0]
+
+			accessDetails, _, err := client.ClientAPI.AccessSessionsAPI.GetAccessSessionAccessDetails(cmd.Context(), accessID).Execute()
+			if err != nil {
+				return err
+			}
+
+			if execute {
+				return executeAccessDetails(cmd.Context(), accessDetails)
+			}
+
+			err = verifyOutputFormatIsSupported(cmd.Context(), client, accessID, outputFormat)
 			if err != nil {
 				return err
 			}
@@ -35,36 +53,23 @@ func AccessDetails() *cobra.Command {
 			var output string
 			switch outputFormat {
 			case "cli":
-				link := accessDetails.GetCli()
-				if accessDetails.GetCli() == "" {
-					return errors.New("no output for format: cli")
+				if accessDetails.GetCli() != "" {
+					output = accessDetails.GetCli()
+				} else {
+					output = accessDetails.Instructions.Plain
 				}
-				output = link
 			case "link":
 				link := accessDetails.GetLink()
-				if link.GetUrl() == "" {
-					return errors.New("no output for format: link")
-				}
 				output = link.GetUrl()
 			case "instructions":
-				if accessDetails.Instructions.Plain == "" {
-					return errors.New("no output for format: instructions")
-				}
 				output = accessDetails.Instructions.Plain
 			case "json":
-				if accessDetails.Json == nil {
-					return errors.New("no output for format: json")
-				}
 				var outputBytes []byte
 				outputBytes, err = json.Marshal(accessDetails.Credentials)
 				if err != nil {
 					return err
 				}
 				output = string(outputBytes)
-			}
-
-			if output == "" {
-				return fmt.Errorf("no output for format: %s", outputFormat)
 			}
 
 			_, err = fmt.Fprintln(cmd.OutOrStdout(), output)
@@ -78,6 +83,35 @@ func AccessDetails() *cobra.Command {
 
 	flags := cmd.Flags()
 	flags.StringVarP(&outputFormat, outputFlagName, "o", "cli", "output format")
+	flags.BoolVarP(&execute, executeFlagName, "r", false, "output format")
 
 	return cmd
+}
+
+func verifyOutputFormatIsSupported(ctx context.Context, client *aponoapi.AponoClient, id string, outputFormat string) error {
+	session, _, err := client.ClientAPI.AccessSessionsAPI.GetAccessSession(ctx, id).Execute()
+	if err != nil {
+		return fmt.Errorf("access session with id %s not found", id)
+	}
+
+	for _, supportedFormat := range session.ConnectionMethods {
+		if supportedFormat == outputFormat {
+			return nil
+		}
+	}
+
+	return fmt.Errorf("unsupported output format: %s. use one of: %s", outputFormat, strings.Join(session.ConnectionMethods, ", "))
+}
+
+func executeAccessDetails(ctx context.Context, accessDetails *clientapi.AccessSessionDetailsClientModel) error {
+	if accessDetails.GetCli() == "" {
+		return errors.New("not executable access details")
+	}
+
+	err := exec.CommandContext(ctx, "sh", "-c", accessDetails.GetCli()).Run() //nolint:gosec // This is a command that should be executed for the user
+	if err != nil {
+		return fmt.Errorf("error executing command:\n%s\n%w", accessDetails.GetCli(), err)
+	}
+
+	return nil
 }
