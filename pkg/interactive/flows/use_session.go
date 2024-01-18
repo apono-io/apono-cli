1e2b67cd2bb653976fa837b63edc407d9b6fe0fc
Tomer Stein <tomer.s@apono.io>
2024-01-18 15:33:31 +0200

    moved use session flow to flows package

---
+++ pkg/interactive/flows/use_session.go
@@ -0,0 +1,76 @@
+package flows
+
+import (
+	"fmt"
+
+	"github.com/apono-io/apono-cli/pkg/aponoapi"
+	"github.com/apono-io/apono-cli/pkg/clientapi"
+	"github.com/apono-io/apono-cli/pkg/interactive/selectors"
+	"github.com/apono-io/apono-cli/pkg/services"
+
+	"github.com/spf13/cobra"
+)
+
+func RunUseSessionInteractiveFlow(cmd *cobra.Command, client *aponoapi.AponoClient) error {
+	session, err := selectors.RunSessionsSelector(cmd.Context(), client)
+	if err != nil {
+		return err
+	}
+
+	if len(session.ConnectionMethods) == 0 {
+		return fmt.Errorf("no connection methods found for session %s", session.Id)
+	}
+
+	if len(session.ConnectionMethods) == 1 {
+		err = printSessionInstructions(cmd, client, session)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	accessMethod, err := selectors.RunSessionCliMethodOptionSelector()
+	if err != nil {
+		return err
+	}
+
+	switch accessMethod {
+	case selectors.ExecuteOption:
+		err = services.ExecuteAccessDetails(cmd, client, session)
+		return err
+
+	case selectors.PrintOption:
+		err = printSessionInstructions(cmd, client, session)
+		return err
+
+	default:
+		return fmt.Errorf("unknown access method %s", accessMethod)
+	}
+}
+
+func printSessionInstructions(cmd *cobra.Command, client *aponoapi.AponoClient, session *clientapi.AccessSessionClientModel) error {
+	accessDetails, err := services.GetSessionDetails(cmd.Context(), client, session.Id, services.InstructionsOutputFormat)
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintln(cmd.OutOrStdout(), accessDetails)
+	if err != nil {
+		return err
+	}
+
+	if !services.IsSessionHaveNewCredentials(session) {
+		err = suggestResetCredentialsCommand(cmd, session.Id)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func suggestResetCredentialsCommand(cmd *cobra.Command, sessionID string) error {
+	_, err := fmt.Fprintf(cmd.OutOrStdout(), "\nTo get new set of credentials, run:\n\n\tapono access reset-credentials %s\n", sessionID)
+	return err
+}
