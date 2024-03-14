12d29972496ccd5fca7b8a34a6af98b13695f3d5
Tomer Stein <60222872+tomerstein@users.noreply.github.com>
2024-03-14 19:34:44 +0200

    added output flag to list access units of access request command (#47)

--- pkg/commands/requests/actions/units.go
+++ pkg/commands/requests/actions/units.go
@@ -3,15 +3,16 @@ package actions
 import (
 	"fmt"
 
+	"github.com/apono-io/apono-cli/pkg/aponoapi"
 	"github.com/apono-io/apono-cli/pkg/services"
+	"github.com/apono-io/apono-cli/pkg/utils"
 
 	"github.com/gosuri/uitable"
 	"github.com/spf13/cobra"
-
-	"github.com/apono-io/apono-cli/pkg/aponoapi"
 )
 
 func AccessUnits() *cobra.Command {
+	format := new(utils.Format)
 	cmd := &cobra.Command{
 		Use:     "units <request_id>",
 		Short:   "Return the access units details for the specified access request",
@@ -27,32 +28,44 @@ func AccessUnits() *cobra.Command {
 			}
 
 			requestID := args[0]
-			return showRequestAccessUnits(cmd, client, requestID)
+			return printRequestAccessUnits(cmd, client, requestID, *format)
 		},
 	}
+
+	flags := cmd.Flags()
+	utils.AddFormatFlag(flags, format)
 
 	return cmd
 }
 
-func showRequestAccessUnits(cmd *cobra.Command, client *aponoapi.AponoClient, requestID string) error {
+func printRequestAccessUnits(cmd *cobra.Command, client *aponoapi.AponoClient, requestID string, format utils.Format) error {
 	requestAccessUnits, err := services.ListAccessRequestAccessUnits(cmd.Context(), client, requestID)
 	if err != nil {
 		return err
 	}
 
-	table := uitable.New()
-	table.AddRow("RESOURCE ID", "RESOURCE TYPE", "RESOURCE NAME", "PERMISSION", "INTEGRATION NAME", "INTEGRATION TYPE")
-	for _, requestAccessUnit := range requestAccessUnits {
-		table.AddRow(
-			requestAccessUnit.Resource.SourceId,
-			requestAccessUnit.Resource.Type.Id,
-			requestAccessUnit.Resource.Name,
-			requestAccessUnit.Permission.Id,
-			requestAccessUnit.Resource.Integration.Name,
-			requestAccessUnit.Resource.Integration.Type,
-		)
-	}
+	switch format {
+	case utils.TableFormat:
+		table := uitable.New()
+		table.AddRow("RESOURCE ID", "RESOURCE TYPE", "RESOURCE NAME", "PERMISSION", "INTEGRATION NAME", "INTEGRATION TYPE")
+		for _, requestAccessUnit := range requestAccessUnits {
+			table.AddRow(
+				requestAccessUnit.Resource.SourceId,
+				requestAccessUnit.Resource.Type.Id,
+				requestAccessUnit.Resource.Name,
+				requestAccessUnit.Permission.Id,
+				requestAccessUnit.Resource.Integration.Name,
+				requestAccessUnit.Resource.Integration.Type,
+			)
+		}
 
-	_, err = fmt.Fprintln(cmd.OutOrStdout(), table)
-	return err
+		_, err := fmt.Fprintln(cmd.OutOrStdout(), table)
+		return err
+	case utils.JSONFormat:
+		return utils.PrintObjectsAsJSON(cmd.OutOrStdout(), requestAccessUnits)
+	case utils.YamlFormat:
+		return utils.PrintObjectsAsYaml(cmd.OutOrStdout(), requestAccessUnits)
+	default:
+		return fmt.Errorf("unsupported output format")
+	}
 }
